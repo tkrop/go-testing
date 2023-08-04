d6f571436553e1a3a72904a79c299de1dac6b64d
tkrop <tronje.krop@zalando.de>
2023-08-04 14:00:11 +0200

    fix: brittle line number in test (#52)

--- test/caller_test.go
+++ test/caller_test.go
@@ -94,7 +94,7 @@ var (
 	}()
 	// CallerTestErrorf provides the file with the line number of the `Errorf`
 	// call in testing.
-	CallerTestErrorf = path.Join(SourceDir, "testing.go:174")
+	CallerTestErrorf = path.Join(SourceDir, "testing.go:175")
 	// CallerGomockErrorf provides the file with the line number of the
 	// `Errorf` call in gomock.
 	CallerGomockErrorf = path.Join(SourceDir, "gomock.go:61")
